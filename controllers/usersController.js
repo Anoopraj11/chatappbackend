4973fc97dd6d3b45b811d978b87e905b040857fe
Anoop rajput <Anooprajpoot955@gmail.com>
2024-06-23 08:35:33 +0530

    upload all files

---
+++ controllers/usersController.js
@@ -0,0 +1,113 @@
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
+
+// register controller
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, password, email } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email already used", status: false });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.checkUsername = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.json({
+        status: false,
+        msg: "Username unavailable.",
+      });
+    } else {
+      return res.json({
+        status: true,
+        msg: "Username available.",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.firebaseLogin = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      delete user.password;
+      return res.json({ status: true, user });
+    } else {
+      return res.json({
+        status: false,
+        msg: "Email not found in database, welcome new user.",
+      });
+    }
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+// login controller
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+//// avatar set
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    });
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+/////// contact routes
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "username",
+      "email",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (ex) {
+    next(ex);
+  }
+};
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messagesRoutes");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Database connection
+const MONGO_URL =
+  process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/chatapp"; // Use either ATLASDB_URL from .env or fallback to local URL
+
+mongoose
+  .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
+
+// Create HTTP server and attach it to Express app
+const server = http.createServer(app);
+
+// Start the server
+const PORT = process.env.PORT || 3000; // Default to port 3000 if PORT is not defined in .env
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+// Initialize Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+// Global object to keep track of online users
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  socket.on("add-user", (userId) => {
+    global.onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = global.onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
